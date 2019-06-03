aa01a3e086de1163362ecf7038c83c875be7d877
Cristian Andres Marin Durango <camd1996@gmail.com>
2019-06-03 14:17:22 -0500

    cambio en fondo de revista

--- src/components/DetailedAnnouncements.vue
+++ src/components/DetailedAnnouncements.vue
@@ -71,6 +71,10 @@
                       </li>
                     </template>
                   </ul>
+                  <div>
+                    <p class="text-left">Compartir en redes sociales</p>
+                    <div class="addthis_inline_share_toolbox_jfcj"></div>
+                  </div>
                 </div>
                 <div class="col-12 col-sm-12 col-md-5 col-lg-4 col-xl-4 d-flex justify-content-center mb-4 order-0 order-md-1">
                   <div class="container-fluid">
